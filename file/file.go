63c9b2ce0d37af2e1800e247d172aff1722bc067
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-05-12 14:20:20 -0400

    update-automated-save

--- file/file.go
+++ file/file.go
@@ -1,8 +1,11 @@
 package file
 
 import (
+	"fmt"
 	"io/fs"
+	"log"
 	"os"
+	"strings"
 )
 
 type File struct {
@@ -22,15 +25,25 @@ type finfo struct {
 	fi   fs.FileInfo
 	err  error
 }
+type db struct {
+	recs []finfo
+}
+func (d *db)Files() []string {
+	result := []string{}
+	for _,v :=range d.recs {
+		result = append(result,v.file)
+	}
+	return result
+}
 
-func (f *File) Exist(dockerFile ...string) []finfo {
-	var db []finfo
+func (f *File) Exist(dockerFile ...string) *db {
+	d := &db{}
 	for _, v := range dockerFile {
 		data, err := os.Stat(v)
-		db = append(db, finfo{file: v, fi: data, err: err})
+		d.recs = append(d.recs, finfo{file: v, fi: data, err: err})
 
 	}
-	return db
+	return d
 }
 
 func (f *File) Handle(dockerFiles ...string) {
@@ -49,4 +62,72 @@ func (f *File) Handle(dockerFiles ...string) {
 	if err != nil {
 		return
 	}
+}
+
+func Read(file string) (string, error) {
+	dat, err := os.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+	return string(dat), err
+}
+
+func Handle(file string) (*os.File, error) {
+	f,err := os.Open(file)
+	return f,err
+}
+
+func HandleAppend(file string) (*os.File, error) {
+	f,err := os.OpenFile(file, os.O_APPEND|os.O_WRONLY, 0644)
+	return f,err
+}
+
+func ReadAll(f *os.File) (string, error) {
+	bytesRead := 20000
+	b := make([]byte, bytesRead)
+	n,err := f.Read(b)
+	if n >= bytesRead{
+		log.Printf("file is huge: %v\n",n)
+	}
+	return string(b[0:n]),err
+}
+
+func GrabTab(s string,tagBegin, tagEnd string) (string,string, error) {
+	split := strings.Split(s, "\n")
+	startLine := 0
+	endLine := 0 
+	for i,v := range split {
+		if strings.Contains(v, tagBegin) {
+			startLine = i
+			break
+		}
+	}
+
+	for i, v := range split {
+		if strings.Contains(v, tagEnd) {
+			endLine = i
+			break
+		}
+	}
+
+	if startLine >= endLine {
+		return s,"",fmt.Errorf("Start > End")
+	}
+	sub := ""
+	sep := ""
+	for i:=0;i<startLine; i++ {
+		sub += split[i]
+		sub = sub + sep + split[i]
+		sep = "\n"
+	}
+
+	tag := ""
+	sep = ""
+	for i:=startLine;i<=endLine; i++ {
+		tag = tag + sep + split[i]
+		sep = "\n"
+	}
+
+	return sub,tag,nil
+
 }
