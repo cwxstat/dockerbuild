91cf74e77480bc586614eaf7e15e051ea768656a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-05-13 17:00:12 -0400

    update-automated-save

---
+++ v1/v1_test.go
@@ -0,0 +1,33 @@
+package v1
+
+import (
+	"github.com/cwxstat/dopt/samples"
+	//	"os"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	type args struct {
+		file string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "V1 test",
+			args: args{
+				file: "Dockerfile.golang.delete.me",
+			},
+		},
+	}
+	for _, tt := range tests {
+		samples.CreateSample(tt.args.file)
+		t.Run(tt.name, func(t *testing.T) {
+			Init(tt.args.file)
+		})
+
+		//os.Remove(tt.args.file)
+	}
+}
