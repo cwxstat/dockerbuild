efa0ede6c1852061d7ca2126e2a6d3fe288d2a47
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-05-14 09:03:42 -0400

    update-automated-save

--- save/save.go
+++ save/save.go
@@ -1,14 +1,21 @@
 package save
 
 import (
+	"github.com/cwxstat/dopt/file"
 	"path/filepath"
+	"errors"
+	"strings"
+	
 
 	"github.com/go-git/go-billy/v5/osfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/filesystem"
+	"time"
 )
 
+var ErrCommit = errors.New("no commit")
 func Init(gitPath string) (*git.Repository, error) {
 	worktreeFS := osfs.New(gitPath)
 	gitDirFS := osfs.New(filepath.Join(gitPath, ".git"))
@@ -16,6 +23,34 @@ func Init(gitPath string) (*git.Repository, error) {
 	return repo, err
 }
 
-func FileSaveCommit(repo *git.Repository, file string) error {
+func FileSaveCommit(repo *git.Repository, repoDir, fileSource, fileDest string) error {
+	
+	file.Copy(fileSource, fileDest)
+	r, err := git.PlainOpen(repoDir)
+	if err != nil {
+		return err
+	}
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	fileCommit := strings.Replace(fileDest,repoDir +"/","",1)
+	if fileCommit == "" {
+		return ErrCommit
+	}
+	_, err = w.Add(fileCommit)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Commit("dopt go-git commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "dopt",
+			Email: "dopt@cwxstat.io",
+			When:  time.Now(),
+		},
+	})
+	return err
 
 }
