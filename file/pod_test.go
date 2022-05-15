db365006043a5ec84175b0317f24c74ef54c11e6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-05-15 16:53:57 -0400

    update-automated-save

--- file/pod_test.go
+++ file/pod_test.go
@@ -19,6 +19,11 @@ func TestFindImages(t *testing.T) {
 		t.Fatalf("deployment: %v", err)
 	}
 
+	// Note: careful
+	if pod.TypeMeta.Kind != "Deployment" {
+		t.FailNow()
+	}
+
 	if pod.Name != "nginx-deployment" {
 		t.Fatalf("pod name from deployment")
 	}
