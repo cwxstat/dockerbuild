358d6e2cb1453efd45eb8c83edd0b55acf5434c0
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-05-15 21:01:12 -0400

    update-automated-save

--- file/file_test.go
+++ file/file_test.go
@@ -2,12 +2,13 @@ package file
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestCopyR(t *testing.T) {
 	os.Remove("./delete.me")
-	os.Remove("/tmp/delete.md")
+	os.Remove("/tmp/delete.me")
 	os.MkdirAll("./delete.me/junk", os.ModePerm)
 	data := []byte("\n\nData\n")
 	os.WriteFile("./delete.me/junk/file", data, 0644)
@@ -18,5 +19,21 @@ func TestCopyR(t *testing.T) {
 	if result != "delete.me/junk/" {
 		t.Errorf("got: %v expected: %v", result, "delete.me/junk/")
 	}
+
+	if !openRead("/tmp/delete.me/junk/file", "Data") {
+		t.Errorf("file not copied")
+	}
+
+}
+
+func openRead(file string, search string) bool {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return false
+	}
+	if strings.Contains(string(data), search) {
+		return true
+	}
+	return false
 
 }
